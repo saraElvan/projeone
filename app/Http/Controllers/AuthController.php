bba3d8c9101ab2f1f58c2277c05552f021c29f0a
saraElvan <saraalwannet@gmail.com>
2026-08-23 17:03:36 +0300

    AuthController icine showLogin ve temel auth metotlari eklendi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    // Giriş (Login) Sayfasını Gösterir
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+
+    // Giriş İşlemini Yapar
+    public function login(Request $request)
+    {
+        //
+    }
+
+    // Kayıt (Register) Sayfasını Gösterir
+    public function showRegister()
+    {
+        return view('auth.register');
+    }
+
+    // Kayıt İşlemini Yapar
+    public function register(Request $request)
+    {
+        //
+    }
+
+    // Çıkış (Logout) İşlemini Yapar
+    public function logout(Request $request)
+    {
+        //
+    }
 }
